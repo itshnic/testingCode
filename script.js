16e7aa9f9683a1f78cfa836df8d53b1cc9e41ac5
Itshnic72 <rv920490@gmail.com>
2023-09-18 16:27:46 +0500

    grid test

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
 "use strict";
-let basket = document.querySelector(".basket__logo_body"),
-	rect = basket.getBoundingClientRect();
-let posLeft = rect.left,
-	posRight = rect.right,
-	posTop = rect.top,
-	posBottom = rect.bottom;
-console.log(posBottom, posTop, posLeft, posRight);
+
